b7a741b50172fb1c27c790af5bf13a3ecea628b6
Manuel Sánchez Suyón <manueduardo000@hotmail.com>
2024-07-15 10:47:06 -0500

    base home, register, login  and components front

--- src/handlers/home.go
+++ src/handlers/home.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 
 	"github.com/ManuEduardo/save-paste/src/views"
+	"github.com/ManuEduardo/save-paste/src/views/components"
 )
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
+	homeProps := views.HomeProps{
+		Items: []components.TagItemProps{
+			{TitleFilter: "Prueba"},
+			{TitleFilter: "Prueba xdd"},
+		},
+	}
 	log.Println("Using Home Handler")
-	views.Home().Render(r.Context(), w)
+	views.Home(homeProps).Render(r.Context(), w)
 }
--- src/views/home_templ.go
+++ src/views/home_templ.go
@@ -8,7 +8,14 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Home() templ.Component {
+import "github.com/ManuEduardo/save-paste/src/views/layouts"
+import "github.com/ManuEduardo/save-paste/src/views/components"
+
+type HomeProps struct {
+	Items []components.TagItemProps
+}
+
+func Home(dataProps HomeProps) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
@@ -38,7 +45,37 @@ func Home() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"min-h-full grow p-2 bg-gradient-to-r from-zinc-50 to-stone-100 dark:bg-gradient-to-r dark:from-sky-800 dark:to-blue-900\"><h2 class=\" text-6xl font-bold text-zinc-800 dark:text-zinc-100 mb-4\">Let's start!</h2><p class=\" w-3/4 mr-0 ml-auto text-1xl text-right font-normal text-zinc-800 dark:text-zinc-100 mb-4\">Save, manage and paste what you need!</p><script>\n\n\t</script></div>")
+			templ_7745c5c3_Var3 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"min-h-full grow p-2 m-2 rounded-lg bg-gradient-to-r from-zinc-50 to-stone-100 dark:bg-gradient-to-r dark:from-sky-800 dark:to-blue-900 drop-shadow\"><h2 class=\" text-4xl font-semibold text-zinc-800 dark:text-zinc-100 mb-4\">Search your save</h2><p class=\" font-normal text-zinc-800 dark:text-zinc-100 mb-4\">Title, content and more...</p><form class=\"flex items-center max-w-lg mx-auto\"><label for=\"voice-search\" class=\"sr-only\">Search</label><div class=\"relative w-full\"><div class=\"absolute inset-y-0 start-0 flex items-center ps-6 pointer-events-none\"><svg class=\"w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white\" aria-hidden=\"tru\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 20 20\"><path stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z\"></path></svg></div><input type=\"text\" id=\"voice-search\" class=\"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-12 p-2.5  dark:bg-blue-950 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow\" placeholder=\"Title, content and more...\" required></div><button class=\"inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-900 rounded-lg border border-blue-700 hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-950 dark:hover:bg-blue-900 dark:focus:ring-blue-950\"><svg class=\"w-4 h-4 mr-1 text-gray-800 dark:text-white\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" fill=\"none\" viewBox=\"0 0 24 24\"><path stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"m8 15 4 4 4-4m0-6-4-4-4 4\"></path></svg>date</button></form><div class=\" flex gap-2 p-1\">")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				templ_7745c5c3_Err = components.TagItem(dataProps.Items[0]).Render(ctx, templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				templ_7745c5c3_Err = components.TagItem(dataProps.Items[1]).Render(ctx, templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><script>\n\n            </script></div>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = layouts.MenuLayout().Render(templ.WithChildren(ctx, templ_7745c5c3_Var3), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
